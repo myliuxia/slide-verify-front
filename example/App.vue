56273f5637501db6dc455596c1dde0a4206571b6
Sum Liu <1285370080@qq.com>
2020-11-20 09:17:35 +0800

    示例

--- example/App.vue
+++ example/App.vue
@@ -1,17 +1,34 @@
 <template>
   <div id="app">
-    <kr-slide-verify
-      :w="slideInfo.w"
-      :h="slideInfo.h"
-      :block_w="slideInfo.block_w"
-      :block_h="slideInfo.block_h"
-      :img="slideInfo.img"
-      :block_img="slideInfo.miniimg"
-      :keyCode="slideInfo.keyCode"
-      @refresh="refresh"
-      @verify="valid"
-      :loading="loading"
-    ></kr-slide-verify>
+    <div class="box">
+      <div class="title">kr-slide-verify 滑块验证示例</div>
+      <div class="verify-box" v-if="verify">
+        <kr-slide-verify
+          :w="slideInfo.w"
+          :h="slideInfo.h"
+          :block_w="slideInfo.block_w"
+          :block_h="slideInfo.block_h"
+          :img="slideInfo.img"
+          :block_img="slideInfo.miniimg"
+          :keyCode="slideInfo.keyCode"
+          @refresh="refresh"
+          @verify="valid"
+          :loading="loading"
+        ></kr-slide-verify>
+      </div>
+      <div v-else class="login">
+        <div class="logo">LOGO</div>
+        <div class="row">
+          <input class="input" placeholder="用户名" :value="'滑块验证'" disabled />
+        </div>
+        <div class="row">
+          <input class="input" type="password" :value="'12345678'" disabled placeholder="密码" />
+        </div>
+        <div class="row">
+          <button class="button" @click="loginVerify">登 陆</button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <script>
@@ -32,12 +49,15 @@ export default {
         id: '',
       },
       loading: false,
+      verify: false,
     }
   },
-  created() {
-    this.getSlideInfo()
-  },
+  created() {},
   methods: {
+    loginVerify() {
+      this.getSlideInfo()
+      this.verify = true
+    },
     // 滑块成功
     async valid(left, callback) {
       axios
@@ -104,6 +124,91 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+.box {
+  width: 1000px;
+  margin: auto;
+  padding: 10px 0;
+  font-family: Arial, Helvetica, sans-serif;
+}
+.title {
+  text-align: center;
+  font-size: 24px;
+  color: #1890ff;
+  margin: 20px;
+  font-weight: bold;
+  padding: 20px 0 40px;
+}
+
+.login {
+  width: 360px;
+  box-sizing: border-box;
+  padding: 20px;
+  margin: auto;
+  border-radius: 4px;
+  box-shadow: 0 0 4px 4px #eeeeee;
+  background: #ffffff;
+  position: relative;
+  .logo {
+    font-size: 24px;
+    color: #1890ff;
+    text-align: center;
+    padding: 20px 0 30px;
+  }
+  .row {
+    height: 40px;
+    margin-bottom: 20px;
+  }
+  .input {
+    -webkit-appearance: none;
+    background-color: #fff;
+    background-image: none;
+    border-radius: 4px;
+    border: 1px solid #dcdfe6;
+    box-sizing: border-box;
+    color: #606266;
+    display: inline-block;
+    font-size: inherit;
+    height: 40px;
+    line-height: 40px;
+    outline: none;
+    padding: 0 15px;
+    transition: border-color 0.2s cubic-bezier(0.645, 0.045, 0.355, 1);
+    width: 100%;
+    font-size: 14px;
+  }
+  .button {
+    width: 100%;
+    display: inline-block;
+    line-height: 1;
+    white-space: nowrap;
+    cursor: pointer;
+    background: #1890ff;
+    border: 1px solid #1890ff;
+    color: #fff;
+    -webkit-appearance: none;
+    text-align: center;
+    box-sizing: border-box;
+    outline: none;
+    margin: 0;
+    transition: 0.1s;
+    font-weight: 500;
+    -moz-user-select: none;
+    -webkit-user-select: none;
+    -ms-user-select: none;
+    padding: 12px 20px;
+    font-size: 14px;
+    border-radius: 4px;
+  }
+}
+
+.verify-box {
+  width: 360px;
+  box-sizing: border-box;
+  padding: 20px;
+  margin: auto;
+  border-radius: 4px;
+  box-shadow: 0 0 4px 4px #eeeeee;
+  background: #ffffff;
 }
 </style>
